e0a8c87eb908e164f184519d4392fdad4f18b2c0
EduardoRFS <theeduardorfs@gmail.com>
2016-08-01 07:20:17 -0300

    Use ua-parser module

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
+'use strict';
+const uaParser = require('ua-parser');
+
 module.exports = function() {
-  var userAgent = new require('./lib/useragent').UserAgent();
-
   return function (ctx, next) {
-    var source =  ctx.request.headers['user-agent'] || '';
-    var ua = userAgent;
+    let source =  ctx.request.headers['user-agent'] || '';
 
-    ua.reset();
-
-    if (typeof source === 'undefined') {
-        source = 'unknown';
-    }
-
-    ua.Agent.source = source.replace(/^\s*/, '').replace(/\s*$/, '');
-    ua.Agent.os = ua.getOS(ua.Agent.source);
-    ua.Agent.platform = ua.getPlatform(ua.Agent.source);
-    ua.Agent.browser = ua.getBrowser(ua.Agent.source);
-    ua.Agent.version = ua.getBrowserVersion(ua.Agent.source);
-    ua.testNginxGeoIP(ctx.request.headers);
-    ua.testBot();
-    ua.testMobile();
-    ua.testAndroidTablet();
-    ua.testTablet();
-    ua.testCompatibilityMode();
-    ua.testSilk();
-    ua.testKindleFire();
-
-    ctx.state.userAgent = ua.Agent;
+    ctx.state.userAgent = uaParser.parse(source);
 
     return next();
   };
